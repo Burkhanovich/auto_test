12b833db5dddb1706cbbd1963d2b810db9f45d45
Burkhanovich <burkhanov1c@gmail.com>
2025-02-24 17:54:49 +0500

    initial

--- apps/testing/models.py
+++ apps/testing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question=models.TextField()
+    image = models.ImageField(upload_to='media/')
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question')
+    answer=models.TextField()
+    boolean_field = models.BooleanField()
+
+
+
+
+
+
